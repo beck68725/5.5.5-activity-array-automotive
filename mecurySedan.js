c7e41be2138da1f5dff62dc24fd8ff25dae4a2c6
Rebecca Coburn <rebeccacoburn@Rebeccas-iMac.local>
2022-05-08 15:14:39 -0500

    update

--- mecurySedan.js
+++ mecurySedan.js
@@ -2,10 +2,10 @@
 const VehicleModule = require("./vehicle")
 
 //this shows how to call from this module...
-// let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
-// console.log(v.make)
+let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
+console.log(v.make)
 
-class Car extends Vehicle {
+class Car extends VehicleModule {
     constructor(make, model, year, color, mileage){
         super(make, model, year, color, mileage);
         this.maximumPassengers = 5;
@@ -26,7 +26,7 @@ class Car extends Vehicle {
                 return this.passenger;
             } 
         } else {
-            console.log(`${this.model} ${this.make} 'is full'`);
+            console.log('This car is full');
         }
     }
 
@@ -34,16 +34,17 @@ class Car extends Vehicle {
         if (this.fuel > 0) {
             console.log("engine started...!!!");
             return this.started = true;
+            
         } else {
             console.log("engine cannot start...");
             return this.started = false;
+            
         }
     }
 
     scheduleService() {
         if (this.mileage > 30000) {
             this.scheduleService = true;
-            console.log('Time for Maintence');
             return this.scheduleService;
         } else {
             console.log('Have a nice trip!')
